48785e3e264816965ca873c3f4387ce4625dc88e
Sebastian Carroll <seabass25@hotmail.com>
2020-04-06 09:46:43 +0100

    Finished parenting

--- src/parenting/Solution.java
+++ src/parenting/Solution.java
@@ -2,6 +2,7 @@ package parenting;
 
 import java.util.*;
 import java.io.*;
+
 public class Solution {
     public static void main(String[] args) {
         Scanner in = new Scanner(new BufferedReader(new InputStreamReader(System.in)));
@@ -9,8 +10,8 @@ public class Solution {
         for (int i = 1; i <= t; ++i) {
             int interval_count = in.nextInt();
             List<Interval> intervals = new ArrayList<>();
-            for (int j=0; j<interval_count; j++) {
-                Interval cur = new Interval(in);
+            for (int j = 0; j < interval_count; j++) {
+                Interval cur = new Interval(in, j);
                 intervals.add(cur);
             }
 
@@ -24,34 +25,84 @@ public class Solution {
             Arrays.fill(combs, 0);
             int[] length = new int[interval_count];
             Arrays.fill(length, 2);
-            nestedLoopOperation(combs, length, 0);
+            int[] solution = nestedLoopOperation(combs, length, intervals);
 
-            System.out.println("Case #" + i + ": " );
+            String output = "";
+            if (solution.length == 0) {
+                output = "IMPOSSIBLE";
+            } else {
+                output = countersToPersons(solution, intervals);
+            }
+            System.out.println("Case #" + i + ": " + output);
         }
     }
 
-    private static void nestedLoopOperation(int[] counters, int[] length, int level) {
-        if(level == counters.length) performOperation(counters);
-        else {
+    private static String countersToPersons(int[] solution, List<Interval> intervals) {
+        String output;
+        String[] personMap = new String[2];
+        personMap[0] = "C";
+        personMap[1] = "J";
+
+        String[] person_ordered = new String[solution.length];
+        for (int i = 0; i < solution.length; i++) {
+            String person = personMap[solution[i]];
+            int location = intervals.get(i).position;
+            person_ordered[location] = person;
+        }
+
+/*        StringBuilder tmp = new StringBuilder();
+
+        for(int s : solution) {
+            tmp.append(personMap[s]);
+        }
+
+        output = tmp.toString();*/
+        return String.join("", person_ordered);
+    }
+
+    private static int[] nestedLoopOperation(int[] counters, int[] length, List<Interval> intervals) {
+        for (int level = 0; level < counters.length; level++) {
             for (counters[level] = 0; counters[level] < length[level]; counters[level]++) {
-                nestedLoopOperation(counters, length, level + 1);
+                if (level == counters.length-1) {
+                    int[] ret = performOperation(counters, intervals);
+                    if (ret.length != 0)
+                        return ret;
+                }
             }
         }
+        return new int[0];
     }
 
-    private static void performOperation(int[] counters) {
-        System.out.println(Arrays.toString(counters));
+    private static int[] performOperation(int[] counters, List<Interval> intervals) {
+        //System.out.println("For counters: " + Arrays.toString(counters));
+        int[] maxes = new int[2];
+        for (int i = 0; i < counters.length; i++) {
+            int person = counters[i];
+            Interval interval = intervals.get(i);
+            int cur_max = maxes[person];
+            //System.out.println("Cur max: " + String.valueOf(cur_max));
+            //System.out.println("Test start: " + String.valueOf(interval.start));
+            if (interval.start >= cur_max) {
+                maxes[person] = interval.end;
+            } else {
+                return new int[0];
+            }
+            //System.out.println(String.valueOf(person) + " has " + interval);
+        }
+        return counters;
     }
 
     private static class Interval {
 
         public int start;
         public int end;
+        public int position;
 
         // Constructor reads two ints from standard in
-        public Interval(Scanner in) {
+        public Interval(Scanner in, int position) {
             start = in.nextInt();
             end = in.nextInt();
+            this.position = position;
         }
 
         public String toString() {
