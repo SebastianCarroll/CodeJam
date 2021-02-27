041c6171fba0887075add47d306d70d59d1a0e8d
Sebastian Carroll <seabass25@hotmail.com>
2021-02-27 12:26:18 +0000

    Completed input/output test

--- app/src/test/java/CodeJam/twentytwenty/squaredance/SolutionTest.java
+++ app/src/test/java/CodeJam/twentytwenty/squaredance/SolutionTest.java
@@ -2,27 +2,42 @@ package CodeJam.twentytwenty.squaredance;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.PrintStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 import static java.util.stream.Collectors.joining;
 
 class SolutionTest {
+
     @Test
-    void canRun() {
-        String input = null;
+    void canRunFromStdINStdOut() {
         try {
-            Path test_file = Paths.get("src/test/java/CodeJam/twentytwenty/squaredance/T-input.txt");
-            System.out.println(test_file.toString());
-            System.out.println("Looking at: " + System.getProperty("user.dir"));
-            input = Files.lines(test_file).collect(joining("\n"));
+            // Set Stdin to file input
+            String dir = "src/test/java/CodeJam/twentytwenty/squaredance/";
+            String testFile = dir + "T-input.txt";
+            System.setIn(new FileInputStream(new File(testFile)));
+
+            // Set stdout to output file
+            String outActual = dir + "T-output.txt";
+            System.setOut(new PrintStream(new File(outActual)));
+
+            Solution sol = new Solution();
+            sol.run();
+
+            String outExpected = dir + "T-output-expected.txt";
+            List<String> actual = Files.readAllLines(Paths.get(outActual));
+            List<String> expected = Files.readAllLines(Paths.get(outExpected));
+            assertIterableEquals(actual, expected, "Expected output files to match");
         } catch (IOException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
-
-        assertNotNull(input);
     }
 }
