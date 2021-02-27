53c598c0680ed8c8f4149ea814f2a95a71c528bb
Sebastian Carroll <seabass25@hotmail.com>
2021-02-27 12:31:00 +0000

    Reformatted code

--- app/src/test/java/CodeJam/twentytwenty/squaredance/SolutionTest.java
+++ app/src/test/java/CodeJam/twentytwenty/squaredance/SolutionTest.java
@@ -12,32 +12,29 @@ import java.nio.file.Paths;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static java.util.stream.Collectors.joining;
 
 class SolutionTest {
 
     @Test
-    void canRunFromStdINStdOut() {
-        try {
-            // Set Stdin to file input
-            String dir = "src/test/java/CodeJam/twentytwenty/squaredance/";
-            String testFile = dir + "T-input.txt";
-            System.setIn(new FileInputStream(new File(testFile)));
+    void canRunFromStdINStdOut() throws IOException {
+        // Set Stdin to file input
+        String dir = "src/test/java/CodeJam/twentytwenty/squaredance/";
+        String testFile = dir + "T-input.txt";
+        System.setIn(new FileInputStream(new File(testFile)));
 
-            // Set stdout to output file
-            String outActual = dir + "T-output.txt";
-            System.setOut(new PrintStream(new File(outActual)));
+        // Set stdout to output file
+        String outActual = dir + "T-output.txt";
+        System.setOut(new PrintStream(new File(outActual)));
 
-            Solution sol = new Solution();
-            sol.run();
+        Solution sol = new Solution();
+        sol.run();
 
-            String outExpected = dir + "T-output-expected.txt";
-            List<String> actual = Files.readAllLines(Paths.get(outActual));
-            List<String> expected = Files.readAllLines(Paths.get(outExpected));
-            assertIterableEquals(actual, expected, "Expected output files to match");
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
+        String outExpected = dir + "T-output-expected.txt";
+        List<String> actual = Files.readAllLines(Paths.get(outActual));
+        Path outExpectedPath = Paths.get(outExpected);
+        List<String> expected = Files.readAllLines(outExpectedPath);
+        assertIterableEquals(actual, expected, "Expected output files to match");
+
+        Files.delete(outExpectedPath);
     }
 }
